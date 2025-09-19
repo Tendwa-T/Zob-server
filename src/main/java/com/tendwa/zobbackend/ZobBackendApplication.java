b75f888239e897bd92067448e45af7676afad12c
Tendwa-T <tirus301@outlook.com>
2025-09-19 17:17:37 +0300

    Initial Setup

---
+++ src/main/java/com/tendwa/zobbackend/ZobBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tendwa.zobbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZobBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZobBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tendwa/zobbackend/ZobBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tendwa.zobbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZobBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
